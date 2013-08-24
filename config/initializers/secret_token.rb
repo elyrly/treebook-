1f6594525cbeb5fe4d47f6dac8cdb50452585ee1
Richard Yang <elyrly@gmail.com>
2013-08-23 22:23:07 -0700

    Created a rail MSG

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '5262bd1d12d6079fdd647f58bdadb956be5ba4b19161717fc3cde7fe90211ea0163ba79183ae68c980b3853dc9b9a1e057926527d49d35de3edf85d72746f1a4'
